c4f53ee8a3e783c801fee301397a0763f914b844
Ryan Oliveira Brito <97217108+ryanoliveiragit@users.noreply.github.com>
2023-01-17 22:01:58 -0300

    finish homepage

--- 02-ignite-timer/src/pages/home/index.tsx
+++ 02-ignite-timer/src/pages/home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             id="task"
             type="text"
             placeholder="De um nome para o seu projeto"
+            list="taskSuggestions"
           />
+          <datalist id="taskSuggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
