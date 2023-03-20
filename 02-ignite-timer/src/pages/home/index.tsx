3a6a18b8cac6cce6aebbe932ee43f1b1f6921668
Ryan Oliveira Brito <97217108+ryanoliveiragit@users.noreply.github.com>
2023-03-19 22:06:58 -0300

    Ciclo completo

--- 02-ignite-timer/src/pages/home/index.tsx
+++ 02-ignite-timer/src/pages/home/index.tsx
@@ -21,7 +21,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(5, "O ciclo precisa ser no minimo de 60 minutos")
+    .min(1, "O ciclo precisa ser no minimo de 1 minutos")
     .max(60, "O ciclo precisa ser no máximo de 60 minutos"),
 });
 
@@ -34,6 +34,7 @@ interface Cycle {
   isActive?: boolean;
   startDate: Date;
   interruptedDate?: Date;
+  finishedDate?: Date;
 }
 
 export function Home() {
@@ -50,22 +51,40 @@ export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   useEffect(() => {
     let interval: number;
 
     if (activeCycle) {
       interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
         );
+
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id == activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          );
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
       }, 1000);
     }
 
     return () => {
       clearInterval(interval);
     };
-  }, [activeCycle]);
+  }, [activeCycle, totalSeconds, activeCycleId]);
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
@@ -87,19 +106,18 @@ export function Home() {
   }
 
   function handleInterruptCycle() {
-    setCycles(
-      cycles.map((cycle) => {
-      if (cycle.id == activeCycleId) {
-        return { ...cycle, interruptedDate: new Date()}
-      } else {
-        return cycle
-      }
-    }),
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id == activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
     );
     setActiveCycleId(null);
   }
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
   const minutesAmount = Math.floor(currentSeconds / 60);
@@ -109,7 +127,7 @@ export function Home() {
   const seconds = String(secondsAmount).padStart(2, "0");
 
   console.log(activeCycle);
-  console.log(cycles)
+  console.log(cycles);
 
   useEffect(() => {
     if (activeCycle) {
@@ -147,7 +165,7 @@ export function Home() {
             placeholder="00"
             disabled={!!activeCycle}
             step={5}
-            min={5}
+            min={1}
             max={60}
             {...register("minutesAmount", { valueAsNumber: true })}
           />
@@ -165,9 +183,9 @@ export function Home() {
 
         {activeCycle ? (
           <StopCountDownButton onClick={handleInterruptCycle} type="button">
-          <HandPalm size={24} />
-          Interromper
-        </StopCountDownButton>
+            <HandPalm size={24} />
+            Interromper
+          </StopCountDownButton>
         ) : (
           <StartCountDownButton disabled={isSubmitDisabled} type="submit">
             <Play size={24} />
