e4215fd941739a15a88a0981ab83705dca3b447e
Ryan Oliveira Brito <ryanoliveirabrito178@gmail.com>
2024-01-22 22:12:24 -0300

    refactor

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,8 +17,8 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
     .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos!')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos!'),
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
